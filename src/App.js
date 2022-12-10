69f2db0a936f872e7d87d40985e0af54d9ce05a7
Valentina <110343367+ValentinaPopko@users.noreply.github.com>
2022-12-10 21:02:53 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  // let poleno = <div>ananas</div>
+  return (
+  <div>
+  <p>Скоро Новый Год</p>
+  <p>Мандарины, ёлка, подарки</p>
+  <p>Осталось 3 недели</p>
+  <input></input>
+  <br/>
+  <input></input>
+  <br/>
+  <input></input>
+  <br/>
+
+  <ul>
+    <li>Скоро Новый Год</li>
+    <li>Скоро Новый Год</li>
+    <li>Скоро Новый Год</li>
+    <li>Скоро Новый Год</li>
+    <li>Скоро Новый Год</li>
+    <li>Скоро Новый Год</li>
+    <li>Скоро Новый Год</li>
+    <li>Скоро Новый Год</li>
+    <li>Скоро Новый Год</li>
+    <li>Скоро Новый Год</li>
+  </ul>
+
+  <tbody>
+    <table border = "1">
+    <tr>
+      <td>Новый Год</td>
+      <td>Новый Год</td>
+      <td>Новый Год</td>
+      
+    </tr>
+    <tr>
+    <td>Новый Год</td>
+    <td>Новый Год</td>
+    <td>Новый Год</td>
+
+    </tr>
+    <tr>
+    <td>Новый Год</td>
+    <td>Новый Год</td>
+    <td>Новый Год</td>
+
+    </tr>
+    </table>
+  </tbody>
+
+  <p className = "new1">Скоро Новый Год</p>
+  <p className = "new2">Мандарины, ёлка, подарки</p>
+  <p className = "new3">Осталось 3 недели</p>
+
+
+  </div>
+  );
+}
+
+export default App;
+
+
